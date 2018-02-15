0d377ddf2c318e1898c354132582c43664167171
IPalaganas <36306667+IPalaganas@users.noreply.github.com>
2018-02-15 10:08:58 -0500

    Add files via upload

---
+++ pbd-solutions/07/YourInitials.java
@@ -0,0 +1,11 @@
+public class YourInitials {
+	public static void main(String[] args ) {
+		System.out.println( "IIIII PPPP" );
+		System.out.println( "  I   P   P" );
+		System.out.println( "  I   P   P" );
+		System.out.println( "  I   PPPP" );
+		System.out.println( "  I   P" );
+		System.out.println( "  I   P" );
+		System.out.println( "IIIII P" );
+	}
+}
