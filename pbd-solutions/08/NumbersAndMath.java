664ad04e45daa9bcf967b76c300208703f957f1d
IPalaganas <36306667+IPalaganas@users.noreply.github.com>
2018-02-16 10:33:23 -0500

    Add files via upload

--- pbd-solutions/08/NumbersAndMath.java
+++ pbd-solutions/08/NumbersAndMath.java
@@ -1,19 +1,33 @@
 public class NumbersAndMath {
 	public static void main(String[] arg )
 	{
+//says the line "this will now count my chickens"
 		System.out.println( "I will now count my chickens:" );
+//says "Hens" then divides 30 by 6 and adds 25 for 30
 		System.out.println( "Hens " + ( 25 + 30 / 6 ) );
+//says roosters then multiples -25 by 3, divides -75 by 4 and gets the remainder of 3 and then subtracts that from 100 so its 97
 		System.out.println( "Roosters " + ( 100 - 25 * 3 % 4 ) );
-		System.out.println( "Now I will ount the eggs:" );
-		System.out.println( 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 );
+//says now i will count your eggs
+		System.out.println( "Now I will count the eggs:" );
+//divides 4 by 2 and gets 0 remainders, divides -1 by 4, then adds 3 + 2 + 1 - 5 + 0 - 1/4 (or 0.25 with using floating point) then adds 6 adding to 7
+		System.out.println( 3 + 2 + 1 - 5 + 4 % 2 - 0.25f + 6 );
+//checks if 5 is less than -2 and says if its true or false 
 		System.out.println( "Is it true that 3 + 2 < 5 - 7?" );
+//calculates if 3+2 and 5-7 and says if it is true or false that 3=2 is greater than 5-7,it is false
 		System.out.println( 3 + 2 < 5 - 7 );
+//asks what 3+2 is then adds them up for 5
 		System.out.println( "What is 3 + 2? " + ( 3 + 2 ) );
+//aks what 5-7 then calculates it for -2
 		System.out.println( "What is 5 - 7? " + ( 5 - 7 ) );
+//says oh thats why its false
 		System.out.println( "Oh, that's why it's false." );
+//says how about some more
 		System.out.println( "How about some more." );
+//asks is it greater then checks if 5 is greater than negative 2 and says true
 		System.out.println( "Is it greater? " + ( 5 > -2 ) );
+//asks if it is greate or equal and checks so that its true since 5 is greater than -2
 		System.out.println( "Is it greater or equal? " + ( 5>= -2 ) );
+//asks if it is less or equal and then checks so that its false
 		System.out.println( "Is it less or equal? " + ( 5<= -2 ) );
 	}
 }
