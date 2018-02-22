d8c41d45b7e22d5ad7db35fdfd8d7926cb0e04d4
IPalaganas <36306667+IPalaganas@users.noreply.github.com>
2018-02-22 10:42:36 -0500

    Add files via upload

---
+++ pbd-solutions/13/YourSchedule.java
@@ -0,0 +1,19 @@
+public class YourSchedule {
+    public static void main(String[] args) {
+    String course1 = "Chemistry";
+    String course2 = "Computer";
+    String course3 = "Biology";
+    String course4 = "Music";
+    String teacher1 = "Ms. Pasquali";
+    String teacher2 = "Mr. Gallo";
+    String teacher3 = "Ms. Mazzacca";
+    String teacher4 = "Ms. Fachinni";
+    
+    System.out.println("+-------------------------------------------------------+");
+    System.out.println("| 1 |                  " + course1 + "|" + teacher1 + "           |");
+    System.out.println("| 2 |                   " + course2 + "|" + teacher2 + "              |");
+    System.out.println("| 3 |                    " + course3 + "|" + teacher3 + "           |");
+    System.out.println("| 4 |                      " + course4 + "|" + teacher4 + "           |");
+    System.out.println("+-------------------------------------------------------+");
+    }
+}
