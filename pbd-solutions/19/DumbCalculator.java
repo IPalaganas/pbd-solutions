d0b3bcf5bfc328df9410b3541eaf5128cc7e6b77
IPalaganas <36306667+IPalaganas@users.noreply.github.com>
2018-02-27 10:33:24 -0500

    Add files via upload

--- pbd-solutions/19/DumbCalculator.java
+++ pbd-solutions/19/DumbCalculator.java
@@ -1,18 +1,13 @@
-import java.util.Scanner;
-
-public class DumbCalculator {
-    public static void main(String[] arg) {
+ import java.util.Scanner;
+ public class DumbCalculator {
+  public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-    
-    System.out.print("What is your first number?");
-    double firstNumber = input.nextDouble;
-    
-    System.out.print("What is your second number?");
-    double secondNumber = input.nextDouble;
-    
-    System.out.print("What is your third number?");
-    double thirdNumber = input.nextDouble;
-    
-    System.out.println("( " + firstNumber + " + " + secondNumber + " + " + thirdNumber + " ) / 2 = " + ((firstNumber + secondNumber + thirdNumber)/ 2) );
-    }
- } 
+    System.out.print("Insert first number:");
+    double firstNumber = input.nextDouble();
+    System.out.print("Insert second number:");
+    double secondNumber = input.nextDouble();
+    System.out.print("Insert third number:");
+    double thirdNumber = input.nextDouble();
+    System.out.println("( " + firstNumber + " + " + secondNumber + " + " + thirdNumber + " ) / 2 = " +  ((firstNumber + secondNumber + thirdNumber) / 2 )); 
+;  }
+}
