dd82de3297cdc5131de2a7665c882e564ca58921
IPalaganas <36306667+IPalaganas@users.noreply.github.com>
2018-03-07 11:00:31 -0500

    Add files via upload

---
+++ pbd-solutions/34/AgeMessages3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class AgeMessages3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+    System.out.print("Name: ");
+    String name = input.next();
+    System.out.print("Age: ");
+    int age = input.nextInt();
+    
+    if (age < 16) {
+        System.out.println("You can't drive, " + name + ".");
+    } if (age == 16 || age == 17) {
+        System.out.println("You can drive, but not vote, " + name + ".");
+    } if (age >= 18 && age <= 24) {
+        System.out.println("You can vote, but not rent a car, " + name + ".");
+    } if (age >= 25) {
+        System.out.println("You can pretty much do anything, " + name + ".");
+        }
+        }
+} 
